a53c7d105246ef544762e4a6f3d5bfe3880df009
AsCrisCorp <cristiano_456@outlook.com>
2026-08-17 00:26:52 -0500

    seccion galeria funcionamiento de modal

---
+++ src/js/app.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+  crearGaleria();
+});
+function crearGaleria() {
+  const galeria = document.querySelector(".galeria-imagenes");
+  const cantidad_imagenes = 16;
+  for (let i = 1; i <= cantidad_imagenes; i++) {
+    const imagen = document.createElement("IMG");
+    imagen.src = `src/img/gallery/full/${i}.jpg`;
+    imagen.alt = "imagen galeria";
+
+    // event handler
+
+    imagen.onclick = function () {
+      mostrarImagen(i);
+    };
+
+    galeria.appendChild(imagen);
+  }
+}
+function mostrarImagen(i) {
+  // agregamos la imagen
+  const imagen = document.createElement("IMG");
+  imagen.src = `src/img/gallery/full/${i}.jpg`;
+  imagen.alt = "imagen galeria";
+  // generar modal
+  const modal = document.createElement("DIV");
+  modal.classList.add("modal");
+  modal.onclick = cerrarModal;
+  modal.appendChild(imagen);
+  // agregar al html
+  const body = document.querySelector("body");
+  body.appendChild(modal);
+}
+
+function cerrarModal() {
+  const modal = document.querySelector(".modal");
+  modal.classList.add("fade-Out");
+  setTimeout(() => {
+    modal?.remove();
+  }, 500);
+}
